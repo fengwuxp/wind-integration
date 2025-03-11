7882275de2e64a971ac9c2e74465adfb122724fa
wuxp <1109695647@qq.com>
2025-03-11 13:46:20 +0800

    fix: AlibabaCloudKmsCredentialsProvider#client 改为延迟初始化

--- wind-kms/alibabacloud/src/main/java/com/wind/integration/alibaba/AlibabaCloudKmsCredentialsProvider.java
+++ wind-kms/alibabacloud/src/main/java/com/wind/integration/alibaba/AlibabaCloudKmsCredentialsProvider.java
@@ -5,6 +5,7 @@ import com.wind.integration.kms.WindCredentialsClient;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.constraints.NotBlank;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * 阿里云 kms 凭据提供者
@@ -15,12 +16,18 @@ import javax.validation.constraints.NotBlank;
 @Slf4j
 public class AlibabaCloudKmsCredentialsProvider implements WindCredentialsProvider {
 
-    private final WindCredentialsClient client = AlibabaCloudKmsCryptoClient.of();
+    private final AtomicReference<WindCredentialsClient> client = new AtomicReference<>();
 
     @Override
     public String getCredentials(@NotBlank String credentialsName) {
         log.debug("get credentials, credentialsName = {}", credentialsName);
-        return client.getCredentialsAsText(credentialsName);
+        synchronized (client) {
+            // 改为延迟初始化，避免不强依赖 kms 时初始化错误
+            if (client.get() == null) {
+                client.set(AlibabaCloudKmsCryptoClient.of());
+            }
+        }
+        return client.get().getCredentialsAsText(credentialsName);
     }
 
 }

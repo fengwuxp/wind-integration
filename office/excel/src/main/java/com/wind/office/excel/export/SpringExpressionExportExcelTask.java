c9683ea38a14047d84d6e3ba3c1ae8b1cd47c86f
wuxp <1109695647@qq.com>
2025-06-03 11:33:16 +0800

    feature: SpringExpressionExportExcelTask#stateSyncer 增加状态同步器

--- office/excel/src/main/java/com/wind/office/excel/export/SpringExpressionExportExcelTask.java
+++ office/excel/src/main/java/com/wind/office/excel/export/SpringExpressionExportExcelTask.java
@@ -6,6 +6,7 @@ import com.wind.office.excel.ExportExcelDataFetcher;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * 基于 spring expression 取值 excel 导出任务，不支持列合并等操作
@@ -18,9 +19,23 @@ public class SpringExpressionExportExcelTask extends AbstractDelegateDocumentTas
 
     private final ExportExcelDataFetcher<?> fetcher;
 
+    /**
+     * 任务状态同步器，用于在分布式情况下通过 redis（缓存） 、或数据库 同步任务的实时状态
+     *
+     * @key 任务 id
+     * @value 任务状态
+     */
+    private final Function<String, OfficeTaskState> stateSyncer;
+
     public SpringExpressionExportExcelTask(ExportExcelTaskInfo taskInfo, ExportExcelDataFetcher<?> fetcher) {
+        this(taskInfo, fetcher, id -> null);
+    }
+
+    public SpringExpressionExportExcelTask(ExportExcelTaskInfo taskInfo, ExportExcelDataFetcher<?> fetcher,
+                                           Function<String, OfficeTaskState> stateSyncer) {
         super(taskInfo);
         this.fetcher = fetcher;
+        this.stateSyncer = stateSyncer;
     }
 
     @Override
@@ -29,6 +44,10 @@ public class SpringExpressionExportExcelTask extends AbstractDelegateDocumentTas
         int queryPage = 1;
         ExportExcelTaskInfo taskInfo = (ExportExcelTaskInfo) getDelegate();
         while (!Thread.currentThread().isInterrupted()) {
+            OfficeTaskState state = stateSyncer.apply(getId());
+            if (state != null) {
+                updateState(state);
+            }
             List rows = fetcher.fetch(queryPage, taskInfo.getFetchSize());
             this.addRows(rows);
             if (OfficeTaskState.isFinished(getState())) {
@@ -41,7 +60,7 @@ public class SpringExpressionExportExcelTask extends AbstractDelegateDocumentTas
             }
             queryPage++;
         }
-        if (Thread.currentThread().isInterrupted()){
+        if (Thread.currentThread().isInterrupted()) {
             log.info("excel task is interrupted，id = {}, state = {}", getId(), getState());
             taskInfo.updateState(OfficeTaskState.INTERRUPT);
         }

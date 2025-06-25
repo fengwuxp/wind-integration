e14d1720b255210b1b069850f4c2a0617a58794f
wuxp <1109695647@qq.com>
2025-06-25 14:28:28 +0800

    refactor(metrics): 更新验证注解为 Jakarta Validation
    - 将 javax.validation.constraints 替换为 jakarta.validation.constraints
    - 涉及多个文件的导入语句更新- 此更改是为了适应 Java EE 向 Jakarta EE 的迁移

--- wind-metrics/src/main/java/com/wind/integration/metrics/fields/WindMetricsFieldFactory.java
+++ wind-metrics/src/main/java/com/wind/integration/metrics/fields/WindMetricsFieldFactory.java
@@ -2,9 +2,9 @@ package com.wind.integration.metrics.fields;
 
 import com.wind.integration.metrics.WindMetricsAggregationQuery;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import java.util.ArrayList;
 import java.util.List;
 

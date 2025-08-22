467f5fa2c2344a70b5b97fd20f155dd044f3e50e
wuxp <1109695647@qq.com>
2025-08-22 14:20:11 +0800

    refactor(money): 重构 Money 类的字符串表示方法
    - 修改 toString 方法，统一处理正数和负数的字符串表示
    - 新增 asNegativeString 方法，专门用于生成负数金额字符串
    - 更新测试用例，使用新的 asNegativeString 方法验证负数金额的字符串表示

--- transaction/money/src/main/java/com/wind/transaction/core/Money.java
+++ transaction/money/src/main/java/com/wind/transaction/core/Money.java
@@ -210,9 +210,13 @@ public final class Money implements Serializable, Comparable<Money> {
 
     @Override
     public String toString() {
-        if (amount < 0) {
-            return String.format("-%s%s", currency.getSign(), this.fen2Yuan().abs());
-        }
         return String.format("%s%s", currency.getSign(), this.fen2Yuan());
+    }
+
+    /**
+     * @return 负数金额字符串
+     */
+    public String asNegativeString() {
+        return String.format("-%s%s", currency.getSign(), this.fen2Yuan());
     }
 }

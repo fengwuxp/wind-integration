f4f6753dbdd2c8af5165262e9c9e1198464d698b
wuxp <1109695647@qq.com>
2025-09-30 15:17:03 +0800

    refactor(mybatis-flex-extends):重构查询助手以支持 WindQuery 和游标分页
    - 引入 WindQuery 和游标分页相关类
    - 更新 from 方法参数类型为 WindQuery- 替换 requireOrderBy 为 shouldOrderBy 方法调用
    - 添加对查询排序类型的非空检查
    - 新增 convert 方法重载以支持游标分页转换
    - 移除不再使用的 Collectors 导入
    - 更新方法注释中的参数名称一致性

--- extends/mybatis-flex-extends/src/main/java/com/wind/mybatis/flex/MybatisQueryHelper.java
+++ extends/mybatis-flex-extends/src/main/java/com/wind/mybatis/flex/MybatisQueryHelper.java
@@ -5,6 +5,9 @@ import com.mybatisflex.core.paginate.Page;
 import com.mybatisflex.core.query.QueryColumn;
 import com.mybatisflex.core.query.QueryOrderBy;
 import com.mybatisflex.core.query.QueryWrapper;
+import com.wind.common.query.WindQuery;
+import com.wind.common.query.cursor.AbstractCursorQuery;
+import com.wind.common.query.cursor.CursorPagination;
 import com.wind.common.query.supports.AbstractPageQuery;
 import com.wind.common.query.supports.Pagination;
 import com.wind.common.query.supports.QueryOrderField;
@@ -13,7 +16,6 @@ import com.wind.common.query.supports.QueryOrderType;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * mybatis flex query helper
@@ -32,6 +34,7 @@ public final class MybatisQueryHelper {
     }
 
     /**
+     * TODO 统一使用 WindQuery ？
      * 通过查询请求创建一个 mybatis flex 分页对象
      *
      * @param query 查询请求
@@ -42,7 +45,7 @@ public final class MybatisQueryHelper {
         return new Page<>(query.getQueryPage(), querySize);
     }
 
-    public static QueryWrapper from(AbstractPageQuery<?> query) {
+    public static QueryWrapper from(WindQuery<? extends QueryOrderField> query) {
         return from(query, null);
     }
 
@@ -54,15 +57,15 @@ public final class MybatisQueryHelper {
      * @param tableAlias 表别名
      * @return QueryWrapper
      */
-    public static QueryWrapper from(AbstractPageQuery<?> query, String tableAlias) {
-        if (query.requireOrderBy()) {
+    public static QueryWrapper from(WindQuery<? extends QueryOrderField> query, String tableAlias) {
+        if (query.shouldOrderBy()) {
             QueryWrapper result = QueryWrapper.create();
             for (int i = 0; i < query.getOrderFields().length; i++) {
                 String orderField = query.getOrderFields()[i].getOrderField();
                 if (tableAlias != null) {
                     orderField = String.format("%s.%s", tableAlias, orderField);
                 }
-                result.orderBy(orderField, Objects.equals(query.getOrderTypes()[i], QueryOrderType.ASC));
+                result.orderBy(orderField, Objects.equals(Objects.requireNonNull(query.getOrderTypes())[i], QueryOrderType.ASC));
             }
             return result;
         }
@@ -97,24 +100,38 @@ public final class MybatisQueryHelper {
      * 将 mybatis plus 的分页对象转换为统一分页对象
      *
      * @param data      mybatis plus 分页数据
-     * @param req       查询请求
+     * @param query     查询请求
      * @param converter 转换实体为 DOT 的函数
      * @return 统一分页对象
      */
-    public static <T, R> Pagination<R> convert(Page<T> data, AbstractPageQuery<?> req, Function<T, R> converter) {
+    public static <T, R> Pagination<R> convert(Page<T> data, AbstractPageQuery<?> query, Function<T, R> converter) {
         List<R> records = data.getRecords().stream().map(converter).toList();
-        return Pagination.of(records, req, data.getTotalRow());
+        return Pagination.of(records, query, data.getTotalRow());
     }
 
     /**
      * 将 mybatis plus 的分页对象转换为统一分页对象
      *
      * @param data    mybatis plus 分页数据
-     * @param req     查询请求
+     * @param query   查询请求
      * @param records DTO 列表
      * @return 统一分页对象
      */
-    public static <T, R> Pagination<R> convert(Page<T> data, AbstractPageQuery<?> req, List<R> records) {
-        return Pagination.of(records, req, data.getTotalRow());
+    public static <T, R> Pagination<R> convert(Page<T> data, AbstractPageQuery<?> query, List<R> records) {
+        return Pagination.of(records, query, data.getTotalRow());
     }
+
+    /**
+     * 将 mybatis plus 的分页对象转换为统一分页对象
+     *
+     * @param data      查询结果数据
+     * @param query     游标查询请求
+     * @param converter 转换实体为 DOT 的函数
+     * @return 统一分页对象
+     */
+    public static <T, R> CursorPagination<R> convert(List<T> data, AbstractCursorQuery<?> query, Function<T, R> converter) {
+        List<R> records = data.stream().map(converter).toList();
+        return CursorPagination.next(records, query);
+    }
+
 }

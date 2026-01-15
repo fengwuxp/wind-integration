9ec70162329e17ba32b0a19f120db7dc7f3aec2d
wuxp <1109695647@qq.com>
2026-01-15 17:26:40 +0800

    chore(pom): springboot 升级到 3.5.9

--- tests/src/test/java/com/wind/integration/infrastructure/redisson/RedissonTemporalSequenceSupportTests.java
+++ tests/src/test/java/com/wind/integration/infrastructure/redisson/RedissonTemporalSequenceSupportTests.java
@@ -1,12 +1,15 @@
 package com.wind.integration.infrastructure.redisson;
 
 import com.wind.sequence.WindSequenceType;
+import com.wind.sequence.time.TemporalSequenceFactory;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.redisson.api.RAtomicLong;
 import org.redisson.api.RedissonClient;
 
@@ -18,30 +21,25 @@ import static org.mockito.ArgumentMatchers.anyString;
  * @author wuxp
  * @date 2023-11-29 14:53
  **/
-class RedissonNumericSequenceGeneratorTest {
+class RedissonTemporalSequenceSupportTests {
 
-    private RedissonNumericSequenceGenerator generator;
 
     @BeforeEach
     void setup() {
-        generator = mock();
+        AtomicLong atomicLong = new AtomicLong(0);
+        RedissonClient client = Mockito.mock(RedissonClient.class);
+        RAtomicLong counter = Mockito.mock(RAtomicLong.class);
+        Mockito.when(counter.incrementAndGet()).thenAnswer((Answer<Long>) invocation -> atomicLong.incrementAndGet());
+        Mockito.when(client.getAtomicLong(anyString())).thenReturn(counter);
+        new RedissonTemporalSequenceSupport(client);
     }
 
     @Test
     void testNext() {
-        String seq = generator.dayNext(ExampleType.SN);
+        String seq = TemporalSequenceFactory.dayNext(ExampleType.SN);
         Assertions.assertTrue(seq.endsWith("000001"));
     }
 
-    private RedissonNumericSequenceGenerator mock() {
-        AtomicLong counter = new AtomicLong(0);
-        RedissonClient redissonClient = Mockito.mock(RedissonClient.class);
-        RAtomicLong atomicLong = Mockito.mock(RAtomicLong.class);
-        Mockito.when(atomicLong.incrementAndGet()).thenAnswer(invocation -> counter.incrementAndGet());
-        Mockito.when(atomicLong.get()).thenAnswer(invocation -> counter.get());
-        Mockito.when(redissonClient.getAtomicLong(anyString())).thenReturn(atomicLong);
-        return new RedissonNumericSequenceGenerator(redissonClient);
-    }
 
     @AllArgsConstructor
     @Getter
@@ -55,7 +53,7 @@ class RedissonNumericSequenceGeneratorTest {
 
 
         @Override
-        public int defaultLength() {
+        public int length() {
             return length;
         }
     }
